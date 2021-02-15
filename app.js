6dfead2ed7f2335ba723c552d3a0ad388aec71d9
mario sousa <vegetalizado@gmail.com>
2021-02-15 11:58:25 +0000

    First views commit

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const helpers = require('handlebars-helpers');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 
 mongoose
@@ -30,9 +34,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      sameSite: true, //regardless if Im using a safe domain or not, it's a safe site to send cookies
+      httpOnly: true,
+      maxAge: 60000
+    },
+    rolling: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 //60sec * 60min * 24h
+    })
+  }));
+
+
 // Express View engine setup
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,7 +60,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Card Game';
 
 
 
