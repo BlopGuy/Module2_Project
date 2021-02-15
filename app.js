85391d41c295dc7da2e02f2daa15cb05f980d51f
BlopGuy <gvieira.assuntos@outlook.pt>
2021-02-15 09:59:12 +0000

    first commit, new file setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/cardcollection', {useNewUrlParser: true})
+  .connect('mongodb://localhost/module2-project', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
