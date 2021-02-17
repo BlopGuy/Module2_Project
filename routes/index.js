7d8693a394a3879d0a564d1c37d37a7a1e88a932
BlopGuy <gvieira.assuntos@outlook.pt>
2021-02-17 14:29:52 +0000

    Navbar and basic page layout complete, login and signup functional

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', { user: req.session.currentUser });
+});
+
+router.get('/gameInfo', (req, res) => {
+  res.render('game-info');
 });
 
 module.exports = router;
